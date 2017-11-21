6c3af59d21613d83f52e65c5170a006352592ac2
John Clarke <cjohn1@vt.edu>
2017-11-21 10:48:53 -0500

    added java classes for the server

---
+++ server/androidserver/Light.java
@@ -0,0 +1,44 @@
+package androidserver;
+
+public class Light
+{
+    private String ipAddress;
+    private boolean isOn;
+    
+    
+    public Light(String ip)
+    {
+        ipAddress = ip;
+        isOn = false;
+    }
+    public boolean isOn()
+    {
+        return isOn;
+    }
+    public void turnOn()
+    {
+        
+    }
+    public void turnOff()
+    {
+        
+    }
+    public String getIpaddress()
+    {
+        return ipAddress;
+    }
+    public boolean equals(Object o)
+    {
+        if(o == null)
+        {
+            return false;
+        }
+        if(o.getClass() == this.getClass())
+        {
+            Light comp = (Light)o;
+            return this.ipAddress.equals(comp.getIpaddress());
+        }
+        return false;
+    }
+    
+}
