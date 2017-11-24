8e4180884da7f469d65e0cf4cec30ea43f6bc8e4
yibing1 <yibing1@vt.edu>
2017-11-24 12:12:21 -0500

    Android

---
+++ Home_automation/app/src/main/java/com/example/prj5/home_automation/Light.java
@@ -0,0 +1,14 @@
+package com.example.prj5.home_automation;
+
+/**
+ * Created by Yibing Zhang on 11/24/2017.
+ */
+
+public class Light {
+    private String ipAddress;
+    private int currentBrightness;
+    public Light(String ipAddress){
+        this.ipAddress=ipAddress;
+    }
+
+}
---
+++ Home_automation/app/src/main/java/com/example/prj5/home_automation/Speaker.java
@@ -0,0 +1,14 @@
+package com.example.prj5.home_automation;
+
+/**
+ * Created by Yibing Zhang on 11/24/2017.
+ */
+
+public class Speaker {
+    private String macAddress;
+
+    public Speaker(String macAddress){
+        this.macAddress=macAddress;
+    }
+
+}
