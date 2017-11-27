e6dda86a14ba2d075ea1ae550c9d2aeae0245e2e
Michael Friend <mrf7@vt.edu>
2017-11-27 00:33:36 -0500

    Fixed light driver for server testing

--- client/raspberryLEDclient.py
+++ client/raspberryLEDclient.py
@@ -1,7 +1,8 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
-from socketIO_client import SocketIO
+from socketIO_client import SocketIO, LoggingNamespace
 import sys
+import logging
 
 
 
@@ -9,18 +10,27 @@ import sys
 
 def turnon():
     GPIO.output(18,GPIO.HIGH)
-    print "LED on"
+	print("LED on")	
+	socketIO.
     
     
 def turnoff():
     GPIO.output(18,GPIO.LOW)
-    print "LED off"
-
+	print("LED off")
+def onConnect(): 
+	print('connected')
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 
+# Set up logger
+logging.getLogger("socketIO-client").setLevel(logging.DEBUG)
+logging.basicConfig()
 #sys.argv[1] is the ip address of the server, sys.argv[2] is the port number
-socketIO = SocketIO(sys.argv[1], sys.argv[2])
+#socketIO = SocketIO(sys.argv[1], sys.argv[2])
+socketIO = SocketIO("localhost", 9092, LoggingNamespace)
 socketIO.on('ON', turnon)
 socketIO.on('OFF', turnoff)
+socketIO.on("connect", onConnect)
+socketIO.emit("message", "mmm")
+socketIO.wait()
