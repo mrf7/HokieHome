08020d18fca2e1aadc47d43f79bea99a0f108656
MRF7 <MRF7@vt.edu>
2017-11-27 07:30:18 +0000

    Uncommented GPIO code on client

--- client/raspberryLEDclient.py
+++ client/raspberryLEDclient.py
@@ -1,4 +1,4 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 from socketIO_client import SocketIO, LoggingNamespace
 import sys
@@ -9,15 +9,15 @@ import logging
 #https://pypi.python.org/pypi/socketIO-client
 
 def turnon():
-    GPIO.output(18,GPIO.HIGH)
+	GPIO.output(18,GPIO.HIGH)
 	print("LED on")	
-	socketIO.
-    
+	    
     
 def turnoff():
-    GPIO.output(18,GPIO.LOW)
+	GPIO.output(18,GPIO.LOW)
 	print("LED off")
 def onConnect(): 
+        socketIO.emit("message", "mmm")
 	print('connected')
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -28,9 +28,8 @@ logging.getLogger("socketIO-client").setLevel(logging.DEBUG)
 logging.basicConfig()
 #sys.argv[1] is the ip address of the server, sys.argv[2] is the port number
 #socketIO = SocketIO(sys.argv[1], sys.argv[2])
-socketIO = SocketIO("localhost", 9092, LoggingNamespace)
+socketIO = SocketIO(sys.argv[1], sys.argv[2], LoggingNamespace)
 socketIO.on('ON', turnon)
 socketIO.on('OFF', turnoff)
 socketIO.on("connect", onConnect)
-socketIO.emit("message", "mmm")
 socketIO.wait()
